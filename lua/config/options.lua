eb810ea9193eb4ffc351de6660b4c263a4c44350
Danni Moiseyev <danni@rustybits.io>
2025-03-24 14:15:14 +0200

    WIP

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -20,7 +20,7 @@ vim.g.clipboard = {
     },
 }
 
-vim.opt_local.expandtab = true
-vim.opt_local.tabstop = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
