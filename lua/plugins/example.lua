f890dc1ca8229c2f63e07369417664a8e42a0ae5
Danni Moiseyev <danni@rustybits.io>
2026-02-09 09:26:38 +0200

    WIP

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -19,17 +19,6 @@ return {
       colorscheme = "catppuccin",
     },
   },
-  {
-      "Exafunction/codeium.nvim",
-      dependencies = {
-          "nvim-lua/plenary.nvim",
-          "hrsh7th/nvim-cmp",
-      },
-      config = function()
-          require("codeium").setup({
-          })
-      end
-  },
    -- change trouble config
   {
     "folke/trouble.nvim",
